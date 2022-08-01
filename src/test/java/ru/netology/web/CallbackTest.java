292e381581cc464782050a70167ee676321c17ce
Galina Demidova <galinagriz77@gmail.com>
2022-08-01 20:12:37 +0300

    commit test +

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,54 @@
+package ru.netology.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CallbackTest {
+    private WebDriver driver;
+    @BeforeAll
+    public static void setUpAll() {
+
+        //System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+     void setUp() {
+        // driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("htt://localhost:9999");
+    }
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver=null;
+    }
+    @Test
+    void shouldTestSomething() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Демидова Галина");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79090039867");
+        driver.findElement(By.cssSelector("label[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();//(By.className("[data-test-id='order-success'] ")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
+    }
+
+
+    }
+
+    
+
